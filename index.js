bb7974b8300218a3299232c1a5677c641c6c732a
Roger <rgr_147@hotmail.com>
2024-10-07 23:10:09 -0300

    projeto desafio classificando nivel do heroi

---
+++ index.js
@@ -0,0 +1,45 @@
+//definindo a classe para criação do herói
+class Heroi {
+    nome;
+    xp;
+
+    constructor(nome,xp) {
+        this.nome = nome;
+        this.xp = xp;
+    }
+    //método que define o nível do herói de acordo com o xp do herói
+    defineNivelDoHeroi() {
+        if(this.xp <= 1000) {
+            return "Ferro";
+        } else if(this.xp <= 2000) {
+            return "Bronze";
+        } else if(this.xp <= 5000) {
+            return "Prata";
+        } else if(this.xp <= 7000) {
+            return "Ouro";
+        } else if(this.xp <= 8000) {
+            return "Platina";
+        } else if(this.xp <= 9000) {
+            return "Ascendente";
+        } else if(this.xp <= 10000) {
+            return "Imortal";
+        } else if(this.xp >= 10001) {
+            return "Radiante";
+        }
+    }
+    //método responsável por mostrar mensagem informando o nível atual do herói 
+    mensagemDeNivel() {
+        const nivel = this.defineNivelDoHeroi();
+        const nome = this.nome;
+        return `O Herói de nome ${nome} está no nível de ${nivel}`;    
+    }
+    //método para upar o nível de xp do herói
+    ganhaXp() {
+        this.xp++;
+    }
+}
+
+const meuHeroi = new Heroi("Heroi Dev", 10000);
+
+meuHeroi.ganhaXp();
+console.log(meuHeroi.mensagemDeNivel());
